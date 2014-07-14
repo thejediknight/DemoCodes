85b6a0b1316677570c125fd6026ff4b7b8d96bf2
thejediknight <hc4342@gmail.com>
2014-07-14 00:00:12 -0700

    Demo Console app to push to github

---
+++ DemoConsoleApp/DemoConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo console application to push to github");
+        }
+    }
+}
