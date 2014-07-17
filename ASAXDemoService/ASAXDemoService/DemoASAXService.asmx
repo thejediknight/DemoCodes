94feffd87d021f7aca033414f8197e0ee932933b
thejediknight <hc4342@gmail.com>
2014-07-17 03:42:49 -0700

    ASAX Demo Service

---
+++ ASAXDemoService/ASAXDemoService/DemoASAXService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="DemoASAXService.asmx.cs" Class="ASAXDemoService.DemoASAXService" %>
---
+++ ASAXDemoService/ASAXDemoService/DemoASAXService.asmx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ASAXDemoService
+{
+    //
+    //ASMX is a legacy technology and is not supported .NEt 4.0 onwards. It is recommened to use WCF for .NET 4.0 and higher.
+    //But, if you stillw ant to use ASMX, use the ASP.NET Empty project and add a ASMX file explicitly.
+    //Note that the result returned to the client is encoded in XML and is not plain text/HTML.
+    //
+
+    /// <summary>
+    /// Summary description for DemoASAXService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class DemoASAXService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        //
+        //When browser calls http://localhost:6833/WebService1.asmx/DummyWebMethod, XML encoded string is returned to the 
+        //browser. 
+        //
+        [WebMethod]
+        public string DummyWebMethod()
+        {
+            return "Dummy Web Method returns this.";
+        }
+    }
+}
