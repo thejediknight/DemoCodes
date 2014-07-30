192ab1b97212f065750260754b5c2ea7b8334728
thejediknight <hc4342@gmail.com>
2014-07-30 12:25:29 +0530

    Data Model Class 'Customer'

---
+++ AzureCustomerServicesProjectBeta/AzureCustomerServiceBeta/CustomerServiceWCFWebRole/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Services.Common;
+using System.Data.Services.Client;
+
+namespace CustomerServiceWCFWebRole
+{
+    [DataServiceKey("PartitionKey", "RowKey")]
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FavoriteMovie { get; set; }
+        public string FavoriteLanguage { get; set; }
+
+
+        private readonly string partitionKey = "customer";
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+
+        public Customer() { }
+
+        public Customer(string firstName, string lastName, string favoriteMovie, string favoriteLanguage)
+        {
+            PartitionKey = partitionKey;
+            RowKey = firstName + " " + lastName;
+
+            FirstName = firstName;
+            LastName = lastName;
+            FavoriteMovie = favoriteMovie;
+            FavoriteLanguage = favoriteLanguage;
+        }
+    }
+}
