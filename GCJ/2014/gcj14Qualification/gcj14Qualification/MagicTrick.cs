1ae172c41ca59522d3aebac9e3e3031e9fa4cf50
Harshal <hc4342@gmail.com>
2014-08-06 12:25:06 +0530

    Qualification Problem A

---
+++ GCJ/2014/gcj14Qualification/gcj14Qualification/MagicTrick.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace gcj14Qualification
+{
+    public class MagicTrick
+    {
+        public MagicTrick()
+        {
+        }
+
+        public string[] solve(string[] lines)
+        {
+            var cases = int.Parse(lines[0]);
+            string[] res = new string[cases];
+            for (int i = 0; i < cases; i++)
+            {
+                var firstAnswer = int.Parse(lines[1 + i * 10]);
+                var firstRow = lines[1 + i * 10 + firstAnswer].Split(' ').Select(x => int.Parse(x));
+                var secondAnswer = int.Parse(lines[6 + i * 10]);
+                var secondRow = lines[6 + i * 10 + secondAnswer].Split(' ').Select(x => int.Parse(x));
+                var intersection = firstRow.Intersect(secondRow).ToList();
+
+                if (intersection.Count == 0)
+                {
+                    res[i] = string.Format("Case #{0}: Volunteer cheated!", i + 1);
+                }
+                else if (intersection.Count == 1)
+                {
+                    res[i] = string.Format("Case #{0}: {1}", i + 1, intersection[0]);
+                }
+                else
+                {
+                    res[i] = string.Format("Case #{0}: Bad Magician!", i + 1);
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ GCJ/2014/gcj14Qualification/gcj14Qualification/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace gcj14Qualification
+{
+    public class Program
+    {
+        public const string PATH_IN = @"E:\GitHub\GCJ\2014\A-small-practice.in";
+        public const string PATH_OUT = @"E:\GitHub\GCJ\2014\A.OUT";
+
+        static void Main(string[] args)
+        {
+            var obj = new MagicTrick();
+            var lines = File.ReadAllLines(PATH_IN);
+            var res = obj.solve(lines);
+            File.WriteAllLines(PATH_OUT, res);
+        }
+    }
+}
